c159851d83319b6e74a550d043a9c2a484369949
Andrew Deitrick <ajdeitrick@gmail.com>
2012-05-14 14:57:51 -0500

    moving code into submission.rb instead of story controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -18,64 +18,36 @@ class StoriesController < ApplicationController
     @lines = Line.find_all_by_story_id(params[:id])
     @submissions = Submission.by_vote.find_all_by_story_id(params[:id])
     @new_submission = Submission.new
-    
-    @vote_total = 10
-    @submissions_votes = Submission.find_all_by_story_id(params[:id])
-    @submissions_votes.each do |submission|
-    @vote_total -= submission.vote 
-    @user=User.find_by_id(params[:user_id])
-    end
-
-    @last_story_object = Story.last
-    @last_story_id = @last_story_object.id
-
+    # To display latest story upon completion of previous story. 
+    @last_story_id = Story.last.id
   end
   
 
 
   def update
+    vote_total = 0
+    submissions = Submission.find_all_by_story_id(params[:id])
+    submissions.each do |submission|
+    vote_total += submission.vote 
+    end
     
-      if session[:vote_time] && (Time.now - session[:vote_time] < 1800)
-        flash[:notice] = "Only 1 Vote Per 30-Minutes...Please!" 
-        redirect_to story_url and return story_url
-      else
-        #@submission = Submission.find_by_id(params[:id])
-        session[:vote_time] = Time.now
-        @vote_total = 0
-        @submissions = Submission.find_all_by_story_id(params[:id])
-        @submissions.each do |submission|
-        @vote_total += submission.vote 
-        end    
-      end
-     
-    if @vote_total == 9 && Line.find_all_by_story_id(params[:id]).count == 9
-      @newlines = Submission.by_vote.find_all_by_story_id(params[:id])
-      @newline = @newlines.first
-      line = Line.new
-      line.content = @newline.content
-      line.story_id = params[:id]
-      line.save
-      Submission.scoped({:conditions => ['story_id = ?', params[:id]]}).destroy_all
-      create_story
-      redirect_to stories_url
-   
-    elsif @vote_total == 9
-      @newlines = Submission.by_vote.find_all_by_story_id(params[:id])
-      @newline = @newlines.first
-      line = Line.new
-      line.content = @newline.content
-      line.story_id = params[:id]
-      line.save
-      Submission.scoped({:conditions => ['story_id = ?', params[:id]]}).destroy_all
-      redirect_to story_url
+    if session[:vote_time] && (Time.now - session[:vote_time] < 1)
+      flash[:notice] = "Only 1 Vote Per 30-Minutes...Please!" 
+      redirect_to story_url and return story_url
+    else
+      #@submission = Submission.find_by_id(params[:id])
+      session[:vote_time] = Time.now    
+    end
+    
+    if vote_total == 9 && Line.find_all_by_story_id(params[:id]).count == 9
+      submission_to_line_create_new_story
+    elsif vote_total == 9
+      submission_to_line
     elsif Submission.find_all_by_story_id(params[:id]).empty? || Submission.find_by_id(params[:submission_id]).nil?
       #FLASH NOTICE
-      redirect_to stories_url
-    else
-      @submission = Submission.find_by_id(params[:submission_id])
-      @submission.vote += params[:vote_count].to_i
-      @submission.save
       redirect_to story_url
+    else
+      submit_vote
     end
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -9,6 +9,7 @@ end
 
 def create_submission
   submission = Submission.new :content => params[:submission][:content], :story_id => params[:id], :vote => 0
+  @user = User.find_by_id(params[:user_id])
   if submission.save
   flash[:save_notice] = "Thanks for submitting!"
   redirect_to story_url
@@ -16,4 +17,38 @@ def create_submission
     flash[:alert] = "Not so fast...try again?"
     redirect_to story_url
   end
+end
+
+# To display count-down of votes
+def votes_left
+  vote_total = 10
+  # @submissions = Submission.find_all_by_story_id(params[:id])
+  @submissions.each do |submission|
+  vote_total -= submission.vote
+  end
+  vote_total
+end
+
+def submit_vote
+  submission = Submission.find_by_id(params[:submission_id])
+  submission.vote += params[:vote_count].to_i
+  submission.save
+  redirect_to story_url
+end
+
+def submission_to_line
+  newlines = Submission.by_vote.find_all_by_story_id(params[:id])
+  newline = newlines.first
+  line = Line.create :content => newline.content, :story_id => params[:id]
+  Submission.scoped({:conditions => ['story_id = ?', params[:id]]}).destroy_all
+  redirect_to story_url
+end
+
+def submission_to_line_create_new_story
+  newlines = Submission.by_vote.find_all_by_story_id(params[:id])
+  newline = newlines.first
+  line = Line.create :content => newline.content, :story_id => params[:id]
+  Submission.scoped({:conditions => ['story_id = ?', params[:id]]}).destroy_all
+  create_story
+  redirect_to story_url  
 end
